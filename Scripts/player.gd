9b7c936fa413b18ea8d4f322fa746769885da4c1
Rudr <kzoldiack@gmail.com>
2026-07-23 18:56:46 +0530

    BombArc

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -13,7 +13,8 @@ var current_state = States.IDLE
 var dir : Vector2
 
 @export var Speed := 100
-
+@export var BombNode : PackedScene
+@export var Muzzle: Marker2D
 
 func _physics_process(delta: float) -> void:
 	_state_handler(delta)
@@ -23,9 +24,14 @@ func _state_handler(delta):
 	match current_state:
 		States.IDLE:_idle_state()
 		States.MOVE:_movemovet_state(delta)
-		States.ATTACK:pass
+		States.ATTACK:_on_attack()
 		States.HURT:pass
 		States.DEAD:pass
+
+
+func _input(event):
+	if event.is_action_pressed("attack"):
+		current_state = States.ATTACK
 
 func _idle_state():
 	if Input.get_vector("left","right","up","down"):
@@ -39,3 +45,12 @@ func _movemovet_state(delta):
 	
 	
 	if !dir: current_state = States.IDLE
+
+
+func _on_attack():
+	current_state = States.ATTACK
+	var bomb = BombNode.instantiate()
+	bomb.position = Muzzle.global_position
+	get_parent().add_child(bomb)
+
+	current_state = States.IDLE
