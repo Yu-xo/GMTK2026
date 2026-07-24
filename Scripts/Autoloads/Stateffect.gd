29504da213b81369c2e694026b656f352e2a549f
Rudr <kzoldiack@gmail.com>
2026-07-24 15:23:12 +0530

    WIP2

--- Scripts/Autoloads/Stateffect.gd
+++ Scripts/Autoloads/Stateffect.gd
@@ -1,23 +1,16 @@
 extends Node
 
-# ==========================
-# PLAYER STATS
-# ==========================
+
 
 var max_hp : int = 100
 var speed : float = 100.0
 var throw_range : float = 200.0
 
-# ==========================
-# PLAYER ABILITIES
-# ==========================
+
 
 var unlock_dash : bool = false
 var unlock_shield : bool = false
 
-# ==========================
-# PROJECTILE UPGRADES
-# ==========================
 
 var volley_count : int = 1
 var double_projectile : bool = false
